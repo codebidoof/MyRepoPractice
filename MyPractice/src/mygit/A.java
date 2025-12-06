3a3cdfa1a3b6531de242d06bdce1c564ad93cff1
princehw03 <princehw03@gmail.com>
2025-12-06 15:59:25 +0900

    Bob이 새로운 버전 생성

--- MyPractice/src/mygit/A.java
+++ MyPractice/src/mygit/A.java
@@ -3,6 +3,8 @@ package mygit;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		B b = new B();
+		b.say();
 	}
 
 }
--- MyPractice/src/mygit/B.java
+++ MyPractice/src/mygit/B.java
@@ -3,5 +3,6 @@ package mygit;
 public class B {
     public void say() {
     	System.out.println("Hello B World");
+    	System.out.println("Hello B Master");
     }
 }
